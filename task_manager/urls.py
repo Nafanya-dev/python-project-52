401131c3c2aa2731fc664bd1a4dd74775cf8145f
Gaevskii.V <gaevskii.v95@gmail.com>
2024-10-01 16:56:40 +0300

    Feat: add home page
    Add route ('/'), view and index.html to display the main page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
 
+from task_manager import views
+
+
 urlpatterns = [
-    path('', lambda request: HttpResponse('Hello')),
+    path('', views.HomeView.as_view()),
     path('admin/', admin.site.urls),
 ]
