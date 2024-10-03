c5cadce6681b5e2eb0dd1846d8263e53a1e99047
Gaevskii.V <gaevskii.v95@gmail.com>
2024-10-03 18:35:11 +0300

    Feat: add the ability to delete a user only with permission

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -5,12 +5,15 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='users-list-page'),
+
     path('create/',
          views.RegisterUserView.as_view(),
          name='register-user-page'),
+
     path('<int:pk>/update/',
          views.UpdateUserView.as_view(),
          name='update-user-page'),
+
     path('<int:pk>/delete/',
          views.DeleteUserView.as_view(),
          name='delete-user-page'),
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth.models import User
+
 from task_manager.users.forms import RegisterUserForm
 
 
@@ -39,8 +44,19 @@ class UpdateUserView(UpdateView):
         return get_object_or_404(User, pk=pk)
 
 
-class DeleteUserView(DetailView):
+class DeleteUserView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = User
     template_name = 'users/delete_user.html'
     context_object_name = 'user'
     success_url = reverse_lazy('users-list-page')
+    login_url = reverse_lazy('login-page')
+
+    def test_func(self):
+        return self.request.user == self.get_object()
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return redirect(self.get_login_url())
+        else:
+            messages.error(self.request, _("You do not have permission to change another user."))
+            return redirect('users-list-page')
