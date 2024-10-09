b852d915bf0c5d50184d39b5e3f438683ab1d90c
Gaevskii.V <gaevskii.v95@gmail.com>
2024-10-09 19:18:09 +0300

    refactor: update translations for texts

--- task_manager/texts.py
+++ task_manager/texts.py
@@ -1,13 +1,28 @@
 from django.utils.translation import gettext_lazy as _
 
-
-# --- USERS ---
-# -- TASK_MANAGER.USERS.VIEWS.PY -- TASK.MANAGER.VIEWS.PY --
+# === GENERAL ===
+# BUTTONS
+SIGN_UP_BUTTON_TEXT = _('Sign up')
+EDIT_BUTTON_TEXT = _('Edit')
+DELETE_BUTTON_TEXT = _("Yes, Delete")
+CREATE_BUTTON_TEXT = _('Create')
 
 # MESSAGES
-PERMISSION_MESSAGE = _("""You do not have permission to change another user.""")
-
 AUTHORIZATION_MESSAGE = _("You are not authorized! Please log in.")
+
+# ----
+
+
+# --- SETTINGS.PY ---
+PASSWORD_VALIDATOR_MESSAGE = _("Your password must contain at least 3 characters.")
+
+# ----
+
+
+# === USERS ===
+# -- TASK_MANAGER.USERS.VIEWS.PY -- TASK.MANAGER.VIEWS.PY --
+# MESSAGES
+PERMISSION_MESSAGE = _("""You do not have permission to change another user.""")
 
 REGISTER_USER_SUCCESS_MESSAGE = _("User successfully registered")
 UPDATE_USER_SUCCESS_MESSAGE = _("User successfully changed")
@@ -17,26 +32,14 @@ LOGIN_SUCCESS_MESSAGE = _("You are logged in")
 LOGOUT_INFO_MESSAGE = _("You are logged out")
 
 
-# BUTTONS
-SIGN_UP_BUTTON_TEXT = _('Sign up')
-EDIT_BUTTON_TEXT = _('Edit')
-DELETE_BUTTON_TEXT = _("Yes, Delete")
-
-
 # TITLES
 USERS_LIST_TITLE_TEXT = _('Users')
 SIGN_UP_TITLE_TEXT = _("Registration")
-UPDATE_TITLE_TEXT = _("Edit user")
-DELETE_TITLE_TEXT = _("Delete user")
-
-
-# --- SETTINGS.PY ---
-
-PASSWORD_VALIDATOR_MESSAGE = _("Your password must contain at least 3 characters.")
+UPDATE_USER_TITLE_TEXT = _("Edit user")
+DELETE_USER_TITLE_TEXT = _("Delete user")
 
 
 # -- USERS FORMS.PY --
-
 # LABELS
 FIRST_NAME_LABEL_USER_FORM = _("First name")
 LAST_NAME_LABEL_USER_FORM = _("Last name")
@@ -47,23 +50,32 @@ PASSWORD2_LABEL_USER_FORM = _("Password confirmation")
 # HELP TEXTS
 USERNAME_HELP_TEXT_USER_FORM = _("Required field. No more than 150 characters. Only letters, numbers and symbols @/./+/-/_.")
 PASSWORD1_HELP_TEXT_USER_FORM = _("Your password must contain at least 3 characters.")
-PASSWORD2_HELP_TEXT_USER_FORM =_("To confirm, please enter your password again.")
+PASSWORD2_HELP_TEXT_USER_FORM = _("To confirm, please enter your password again.")
 
 # MESSAGES
 USERNAME_VALIDATION_ERROR_MESSAGE_USER_FORM = _("A user with that username already exists.")
 
+# ----
 
-# --- STATUSES ---
+
+# === STATUSES ===
 # -- TASK_MANAGER.STATUSES.VIEWS.PY --
+CREATE_STATUS_TEXT = _('Create Status')
 
-# BUTTONS
-CREATE_STATUS_BUTTON_TEXT = _('Create Status')
+# MESSAGES
+CREATE_STATUS_SUCCESS_MESSAGE = _('Status successfully created')
+UPDATE_STATUS_SUCCESS_MESSAGE = _('Status successfully changed')
+DELETE_STATUS_SUCCESS_MESSAGE = _('Status successfully deleted')
+
 
 # TITLES
 STATUSES_LIST_TITLE_TEXT = _('Statuses')
-
+UPDATE_STATUS_TITLE_TEXT = _('Change Status')
+DELETE_STATUS_TITLE_TEXT = _('Delete status')
 
 # -- TASK_MANAGER.STATUSES.FORMS.PY --
 
 # LABELS
 NAME_LABEL_STATUS_FORM = _('Name')
+
+# ----
